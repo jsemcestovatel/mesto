6f29749d6ae364ddc7c399c349f7e761b07d8ba8
Anatoliy Loskutov <loskutov.tolik@gmail.com>
2022-06-28 16:21:58 +0200

    refactor: object oriented programming

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -2,32 +2,32 @@
 export const initialCards = [
   {
     name: "Учебная литература",
-    link: "https://jsemcestovatel.github.io/mesto/images/img01.jpg",
+    link: "https://jsemcestovatel.github.io/mesto/src/images/img01.jpg",
     alt: "Иллюстрация Учебная литература",
   },
   {
     name: "JavaScript",
-    link: "https://jsemcestovatel.github.io/mesto/images/img02.jpg",
+    link: "https://jsemcestovatel.github.io/mesto/src/images/img02.jpg",
     alt: "Иллюстрация JavaScript",
   },
   {
     name: "Прототипы",
-    link: "https://jsemcestovatel.github.io/mesto/images/img03.jpg",
+    link: "https://jsemcestovatel.github.io/mesto/src/images/img03.jpg",
     alt: "Иллюстрация Прототипы",
   },
   {
     name: "Coding",
-    link: "https://jsemcestovatel.github.io/mesto/images/img04.jpg",
+    link: "https://jsemcestovatel.github.io/mesto/src/images/img04.jpg",
     alt: "Иллюстрация Coding",
   },
   {
     name: "Debugging",
-    link: "https://jsemcestovatel.github.io/mesto/images/img05.jpg",
+    link: "https://jsemcestovatel.github.io/mesto/src/images/img05.jpg",
     alt: "Иллюстрация Debugging",
   },
   {
     name: "Проекты",
-    link: "https://jsemcestovatel.github.io/mesto/images/img06.jpg",
+    link: "https://jsemcestovatel.github.io/mesto/src/images/img06.jpg",
     alt: "Иллюстрация Проекты",
   },
 ];
