502550983cc951860775d95d82c23c1c81af675e
Anatoliy Loskutov <loskutov.tolik@gmail.com>
2022-06-29 16:06:57 +0200

    fix: first review

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,17 +3,16 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(settings, popupElement) {
     super(popupElement);
-    this._popupShowElement = this._popup;
-    this._popupShowElementImage = this._popupShowElement.querySelector(
+    this._popupShowElementImage = this._popup.querySelector(
       settings.popupShowElementImage
     );
-    this._popupShowElementTitle = this._popupShowElement.querySelector(
+    this._popupShowElementTitle = this._popup.querySelector(
       settings.popupShowElementTitle
     );
   }
 
   open(elementData) {
-    this._popupShowElementImage.src = elementData.link;;
+    this._popupShowElementImage.src = elementData.link;
     this._popupShowElementImage.alt = elementData.alt;
     this._popupShowElementTitle.textContent = elementData.name;
     super.open();
